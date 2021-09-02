0a8d8b687c5ec34d3f2839d6942db552051af16a
Roccofranchini <rocco.franchini1@gmail.com>
2021-09-02 09:49:30 +0200

    fx

--- js/script.js
+++ js/script.js
@@ -5,16 +5,7 @@ Vue.config.devtools = true;
 const app = new Vue({
 	el: "#root",
 	data: {
-		mail1: null,
-		mail2: null,
-		mail3: null,
-		mail4: null,
-		mail5: null,
-		mail6: null,
-		mail7: null,
-		mail8: null,
-		mail9: null,
-		mail10: null,
+		mails: [],
 	},
 	methods: {
 		getRndomMail(mail) {
@@ -26,15 +17,15 @@ const app = new Vue({
 		},
 	},
 	created() {
-		this.getRndomMail("mail1");
-		this.getRndomMail("mail2");
-		this.getRndomMail("mail3");
-		this.getRndomMail("mail4");
-		this.getRndomMail("mail5");
-		this.getRndomMail("mail6");
-		this.getRndomMail("mail7");
-		this.getRndomMail("mail8");
-		this.getRndomMail("mail9");
-		this.getRndomMail("mail10");
+		for (let i = 0; i < 10; i++)
+			axios
+				.get("https://flynn.boolean.careers/exercises/api/random/mail")
+				.then((res) => {
+					const mail = res.data.response;
+					this.mails.push(mail);
+				})
+				.catch((error) => {
+					this.error = true;
+				});
 	},
 });
